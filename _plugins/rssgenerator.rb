c6cabfd1797bb5aaf857b4c9037f0a45d8ec7f51
mac <mac@ozgrozer.com>
2017-09-17 16:59:08 +0300

    Tabs converted to spaces.

--- _plugins/rssgenerator.rb
+++ _plugins/rssgenerator.rb
@@ -21,97 +21,97 @@
 # Copyright Assaf Gelber 2014
 
 module Jekyll
-	class RssFeed < Page; end
+  class RssFeed < Page; end
 
-	class RssGenerator < Generator
-		priority :low
-		safe true
+  class RssGenerator < Generator
+    priority :low
+    safe true
 
-		# Generates an rss 2.0 feed
-		#
-		# site - the site
-		#
-		# Returns nothing
-		def generate(site)
-			require 'rss'
-			require 'cgi/util'
+    # Generates an rss 2.0 feed
+    #
+    # site - the site
+    #
+    # Returns nothing
+    def generate(site)
+      require 'rss'
+      require 'cgi/util'
 
-			# Create the rss with the help of the RSS module
-			rss = RSS::Maker.make("2.0") do |maker|
-				maker.channel.title = site.config['name']
-				maker.channel.link = site.config['baseurl']
-				maker.channel.description = site.config['description'] || "RSS feed for #{site.config['name']}"
-				maker.channel.author = site.config["author"]
-				maker.channel.updated = site.posts.docs.map { |p| p.date  }.max
-				maker.channel.copyright = site.config['copyright']
+      # Create the rss with the help of the RSS module
+      rss = RSS::Maker.make("2.0") do |maker|
+        maker.channel.title = site.config['name']
+        maker.channel.link = site.config['baseurl']
+        maker.channel.description = site.config['description'] || "RSS feed for #{site.config['name']}"
+        maker.channel.author = site.config["author"]
+        maker.channel.updated = site.posts.docs.map { |p| p.date  }.max
+        maker.channel.copyright = site.config['copyright']
 
-				post_limit = site.config['rss_post_limit'].nil? ? site.posts.docs.count : site.config['rss_post_limit'] - 1
+        post_limit = site.config['rss_post_limit'].nil? ? site.posts.docs.count : site.config['rss_post_limit'] - 1
 
-				site.posts.docs.reverse[0..post_limit].each do |doc|
-					doc.read
-					maker.items.new_item do |item|
-						item.title = doc.data['title']
-						link = "#{site.config['baseurl']}#{doc.url}"
-						item.guid.content = link
-						item.link = link
-						item.description = "<![CDATA[" + doc.data['excerpt'].to_s.gsub("\n", ' ').gsub(%r{</?[^>]+?>}, '').strip[0...150] + "]]>"
-						item.updated = doc.date
-					end
-				end
-			end
+        site.posts.docs.reverse[0..post_limit].each do |doc|
+          doc.read
+          maker.items.new_item do |item|
+            item.title = doc.data['title']
+            link = "#{site.config['baseurl']}#{doc.url}"
+            item.guid.content = link
+            item.link = link
+            item.description = "<![CDATA[" + doc.data['excerpt'].to_s.gsub("\n", ' ').gsub(%r{</?[^>]+?>}, '').strip[0...150] + "]]>"
+            item.updated = doc.date
+          end
+        end
+      end
 
-			# File creation and writing
-			rss_path = ensure_slashes(site.config['rss_path'] || "/")
-			rss_name = site.config['rss_name'] || "rss.xml"
-			full_path = File.join(site.dest, rss_path)
-			ensure_dir(full_path)
+      # File creation and writing
+      rss_path = ensure_slashes(site.config['rss_path'] || "/")
+      rss_name = site.config['rss_name'] || "rss.xml"
+      full_path = File.join(site.dest, rss_path)
+      ensure_dir(full_path)
 
-			# We only have HTML in our content_encoded field which is surrounded by CDATA.
-			# So it should be safe to unescape the HTML.
-			feed = CGI::unescapeHTML(rss.to_s)
+      # We only have HTML in our content_encoded field which is surrounded by CDATA.
+      # So it should be safe to unescape the HTML.
+      feed = CGI::unescapeHTML(rss.to_s)
 
-			File.open("#{full_path}#{rss_name}", "w") { |f| f.write(feed) }
+      File.open("#{full_path}#{rss_name}", "w") { |f| f.write(feed) }
 
-			# Add the feed page to the site pages
-			site.pages << Jekyll::RssFeed.new(site, site.dest, rss_path, rss_name)
-		end
+      # Add the feed page to the site pages
+      site.pages << Jekyll::RssFeed.new(site, site.dest, rss_path, rss_name)
+    end
 
-		private
+    private
 
-		# Ensures the given path has leading and trailing slashes
-		#
-		# path - the string path
-		#
-		# Return the path with leading and trailing slashes
-		def ensure_slashes(path)
-			ensure_leading_slash(ensure_trailing_slash(path))
-		end
+    # Ensures the given path has leading and trailing slashes
+    #
+    # path - the string path
+    #
+    # Return the path with leading and trailing slashes
+    def ensure_slashes(path)
+      ensure_leading_slash(ensure_trailing_slash(path))
+    end
 
-		# Ensures the given path has a leading slash
-		#
-		# path - the string path
-		#
-		# Returns the path with a leading slash
-		def ensure_leading_slash(path)
-			path[0] == "/" ? path : "/#{path}"
-		end
+    # Ensures the given path has a leading slash
+    #
+    # path - the string path
+    #
+    # Returns the path with a leading slash
+    def ensure_leading_slash(path)
+      path[0] == "/" ? path : "/#{path}"
+    end
 
-		# Ensures the given path has a trailing slash
-		#
-		# path - the string path
-		#
-		# Returns the path with a trailing slash
-		def ensure_trailing_slash(path)
-			path[-1] == "/" ? path : "#{path}/"
-		end
+    # Ensures the given path has a trailing slash
+    #
+    # path - the string path
+    #
+    # Returns the path with a trailing slash
+    def ensure_trailing_slash(path)
+      path[-1] == "/" ? path : "#{path}/"
+    end
 
-		# Ensures the given directory exists
-		#
-		# path - the string path of the directory
-		#
-		# Returns nothing
-		def ensure_dir(path)
-			FileUtils.mkdir_p(path)
-		end
-	end
+    # Ensures the given directory exists
+    #
+    # path - the string path of the directory
+    #
+    # Returns nothing
+    def ensure_dir(path)
+      FileUtils.mkdir_p(path)
+    end
+  end
 end
